c2cf14a7f4a34b394895856fcae668566ef56b10
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-01 18:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", trigger);
+
+async function trigger() {
+    let txt = document.getElementById("text").value;
+    let dly = document.getElementById("delay").value;
+
+    // await promise
+    let result = await delayText(txt, dly);
+
+    document.getElementById("output").innerText = result;
+}
+
+function delayText(text, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(text);
+        }, Number(delay));
+    });
+}
